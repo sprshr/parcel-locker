125ede13b975cd97d2032054cdd6ce7984d2702f
Sepehr Sahraian <sep3831@gmail.com>
2022-03-12 19:58:15 -0800

    Add is_drop_off()

--- locker.py
+++ locker.py
@@ -1,14 +1,16 @@
+import csv
 class Locker:
-    def __init__(self):
-        self.couriers ={
-            "FedEx" : 1,
-            "UPS" : 2,
-            "USPS" : 3
-        }
-
-    def drop_off(self, courierCode):
+    couriers = {
+        "FedEx" : 1,
+        "UPS" : 2,
+        "USPS" : 3
+    }
+    def is_drop_off(self, courierCode):
         for key in self.couriers:
             if self.couriers[key] == courierCode:
-                return key
-                break
+                self.courier = key
+                return True
         else: return False
+    
+    def drop_off(self, recepient):
+        pass
--- test.py
+++ test.py
@@ -1,20 +1,26 @@
 import unittest
 from locker import *
-
-
 class TestLocker(unittest.TestCase):
     def setUp(self):
         self.fedex = Locker()
         self.ups = Locker()
         self.usps = Locker()
-        self.wrong_package = Locker()
+        self.wrongParcel = Locker()
 
-    def test_drop_off(self):
-        self.assertEqual(self.fedex.drop_off(1), 'FedEx')
-        self.assertEqual(self.ups.drop_off(2), "UPS")
-        self.assertEqual(self.usps.drop_off(3), "USPS")
-        self.assertFalse(self.wrong_package.drop_off(454))
+    def test_is_drop_off(self):
+        self.assertTrue(self.fedex.is_drop_off(1))
+        self.assertEqual(self.fedex.courier, "FedEx")
+        self.assertTrue(self.ups.is_drop_off(2))
+        self.assertEqual(self.ups.courier, "UPS")
+        self.assertTrue(self.usps.is_drop_off(3))
+        self.assertEqual(self.usps.courier, "USPS")
+        self.assertFalse(self.wrongParcel.is_drop_off(444))
 
+    def tearDown(self):
+        del self.fedex
+        del self.ups
+        del self.usps
+        del self.wrongParcel
 
 if __name__ == "__main__":
     unittest.main()
