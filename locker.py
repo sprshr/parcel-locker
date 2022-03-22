e380b4aef636d98ee4ba6c93a5482a77e4510ded
Sepehr Sahraian <68083280+sprshr@users.noreply.github.com>
2022-03-22 17:26:20 +0000

    Add Locker.create() to initiate database

--- locker.py
+++ locker.py
@@ -1,16 +1,38 @@
-import csv
+import sqlite3 as sq
+
+databasePath = 'locker_log.db'
+
+
 class Locker:
-    couriers = {
-        "FedEx" : 11111,
-        "UPS" : 22222,
-        "USPS" : 33333
-    }
+    couriers = {"FedEx": 11111, "UPS": 22222, "USPS": 33333}
+    exists = False
+    #create database table
+    @classmethod
+    def create(self):
+        try:
+            self.conn = sq.connect(databasePath)
+            self.cursor = self.conn.cursor()
+            self.cursor.execute("""CREATE TABLE locker_log(
+                               firstName text,
+                               lastName text,
+                               address text,
+                               zipCode int,
+                               pickUpCode int,
+                               item text
+            )""")
+        except sq.Error:
+            #exists is for testing purposes
+            Locker.exists = True
+        finally:
+            Locker.exists = True
+
     def is_drop_off(self, courierCode):
         for key in self.couriers:
             if self.couriers[key] == courierCode:
                 self.courier = key
                 return True
-        else: return False
-    
+        else:
+            return False
+
     def drop_off(self, first, last, address, zipCode):
         pass
--- test.py
+++ test.py
@@ -1,12 +1,22 @@
+#Avoid running the test to keep the database file!!!!!
+#This test deletes the database file at /locker_log.db!!!!!
+
 import unittest
-from locker import *
-from input import Input
+import os
+from locker import Locker
+
+
 class TestLocker(unittest.TestCase):
     def setUp(self):
         self.fedex = Locker()
         self.ups = Locker()
         self.usps = Locker()
         self.wrongParcel = Locker()
+        try:
+            os.remove('locker_log.db')
+        except FileNotFoundError:
+            pass
+        Locker.create()
 
     def test_is_drop_off(self):
         self.assertTrue(self.fedex.is_drop_off(11111))
@@ -17,11 +27,17 @@ class TestLocker(unittest.TestCase):
         self.assertEqual(self.usps.courier, "USPS")
         self.assertFalse(self.wrongParcel.is_drop_off(343654))
 
+    def test_create(self):
+        self.assertTrue(Locker.exists)
+        self.assertTrue(os.path.isfile('locker_log.db'))
+
     def tearDown(self):
         del self.fedex
         del self.ups
         del self.usps
         del self.wrongParcel
+        #os.remove("locker_log.db")
+
 
 if __name__ == "__main__":
     unittest.main()
