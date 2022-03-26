1ecc040457c84780284ba4aa62d2bb528df8c4b7
Sepehr Sahraian <sep3831@gmail.com>
2022-03-26 01:47:25 -0700

    BugFix

--- input.py
+++ input.py
@@ -2,14 +2,13 @@ class Input:
     @classmethod
     def numeric(cls, errorMessage, minDigit = None, maxDigit = None):
         while True:
-            while True:
-                try:
-                    value = int(input())
-                except ValueError or TypeError:
-                    print(errorMessage)
-                else: break
-            if minDigit != None and len(str(value)) < minDigit:
+            try:
+                value = int(input())
+            except ValueError or TypeError:
                 print(errorMessage)
-            elif maxDigit != None and len(str(value)) > maxDigit:
-                print(errorMessage)
-            else: return value
+            else: break
+        if minDigit != None and len(str(value)) < minDigit:
+            print(errorMessage)
+        elif maxDigit != None and len(str(value)) > maxDigit:
+            print(errorMessage)
+        else: return value
