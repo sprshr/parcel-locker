d19ecf9f5bc07c75fc62037ba04eeae2985d48b0
Sepehr Sahraian <68083280+sprshr@users.noreply.github.com>
2022-03-30 20:28:51 +0000

    Create telegram bot

---
+++ telegram.py
@@ -0,0 +1,18 @@
+import requests
+
+telegramURL = "https://api.telegram.org/bot{token}/{method}"
+
+class Bot():
+    def __init__(self, APItoken:str):
+        self.token = APItoken
+
+    def send_message(self, chatID:str, message:str):
+        pload = {
+            'chat_id' : chatID,
+            'text' : message,
+            'parse_mode' : "HTML"
+        }
+        #print(telegramURL.format(token = self.token, method = "sendMessage"))
+        #print(pload)
+        r = requests.post(telegramURL.format(token = self.token, method = "sendMessage"), data = pload)
+        #print(r.json)
